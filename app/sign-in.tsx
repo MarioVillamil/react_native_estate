75362771ad790e3aa794d4bdeca84d2ac6012add
Mario Villamil <MAVIL177@ulaval.ca>
2025-02-19 19:36:48 -0500

    Initial commit

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View} from "react-native";
+import {Text} from "react-native";
+
+function SignIn() {
+    return (
+        <View>
+            <Text>Sign-in</Text>
+        </View>
+    )
+}
+
+export default SignIn
